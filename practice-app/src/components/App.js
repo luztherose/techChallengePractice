3e077c529b56655f8e61b0b34d13594bca94ceea
luztherose <luzdelarosaramirez@gmail.com>
2020-11-09 14:54:33 -0500

    update the App component

--- practice-app/src/components/App.js
+++ practice-app/src/components/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 import {
   BrowserRouter as Router,
-  Route, Link
+  Route
 }from 'react-router-dom';
-import LogIn from "./LogIn";
 import '../styles/App.scss';
-import Login from "./LogIn";
-import Users from "./Users"
+import LogIn from "./LogIn";
 import SignUp from "./SignUp"
-import LogOut from "./LogOut"
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="app wrapper">
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={LogIn} />
           <Route path="/signup" component={SignUp} />
         </div>
       </Router>
