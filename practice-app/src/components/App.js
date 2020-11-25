7a61bd0d2d072bd6f9cc30772ef98e2f5339efa9
luztherose <luzdelarosaramirez@gmail.com>
2020-11-25 10:58:16 -0500

    sign in page done

--- practice-app/src/components/App.js
+++ practice-app/src/components/App.js
@@ -5,7 +5,8 @@ import {
 }from 'react-router-dom';
 import '../styles/App.scss';
 import LogIn from "./LogIn";
-import SignUp from "./SignUp"
+import SignUp from "./SignUp";
+import Users from "./Users"
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <div className="app wrapper">
           <Route exact path="/" component={LogIn} />
           <Route path="/signup" component={SignUp} />
+          <Route path="/users" component={Users} />
         </div>
       </HashRouter>
     );
--- practice-app/src/components/LogIn.js
+++ practice-app/src/components/LogIn.js
@@ -1,17 +1,54 @@
+import axios from "axios";
 import React, { Component } from "react";
 import {Link} from "react-router-dom";
+import { createHashHistory } from "history"
 
 class Longin extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleChange = (event) => {
+        const user = this.state;
+        const credentials = event.target.name;
+        if(credentials === "userEmail") {
+            user.username = event.target.value;
+        }else if(credentials === "password") {
+            user.password = event.target.value;
+        }
+
+        this.setState(user);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post(`http://51.38.51.187:3333/api/v1/auth/login`, this.state).then(res => {
+            localStorage.setItem('token', res.data.accessToken);
+            const history = createHashHistory()
+            history.push("/users")
+        }).catch(error => {
+            alert(error.response.data.message);
+            console.log(error);
+        })
+    }
+
     render() {
+        if(localStorage.getItem('token') !== null) {
+            const history = createHashHistory()
+            history.push("/users")
+        }
+
         return (
             <div className="formContainer">
                 <h1>Sign In</h1>
                 <form method="post" action="/login">
                     <label>Email Address </label>
-                    <input type="email"name="userEmail" placeholder="johnsmith@gmail.com"></input>
+                    <input type="email"name="userEmail" placeholder="johnsmith@gmail.com" onChange={this.handleChange}></input>
                     <label>Password </label>
-                    <input type="password" name="password"></input>
-                    <input type="submit" value="Sign In!"></input>
+                    <input type="password" name="password" onChange={this.handleChange}></input>
+                    <input type="submit" value="Sign In!" onClick={this.handleSubmit}></input>
                 </form>
                 <div>
                     <p>Please Sign Up!</p>
